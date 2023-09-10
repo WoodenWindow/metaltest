127019982569bb9223e351167f65cbdbea69f158
WoodenWindow <officialadsh@gmail.com>
2023-09-10 21:54:49 +0530

    Added subtraction example

--- hello_metal.c
+++ hello_metal.c
@@ -12,7 +12,10 @@ int main() {
 	int b = 20;
 	printf("Sum is %d", a + b);
 
-	// Please add subtraction example
+	// Added subtraction example
+	int c = 20;
+	int d = 30;
+	printf("Minus is %d", d - c);
 
 	return 0;
 }
