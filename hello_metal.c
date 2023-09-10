752a782921af8bf2872ce01be6b5b67d3d7dc668
WoodenWindow <officialadsh@gmail.com>
2023-09-10 14:57:10 +0530

    Resolved merge conflicts

--- hello_metal.c
+++ hello_metal.c
@@ -7,6 +7,10 @@ int main() {
 	// Comment added by Wooden
 	printf("Hello Wooden");
 
-	// please add arithmetic addition example
+	// Added addition example
+	int a = 10;
+	int b = 20;
+	printf("Sum is %d", a + b);
+
 	return 0;
 }
